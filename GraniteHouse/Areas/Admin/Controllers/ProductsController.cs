ccba12c72cacb1bc08c60995ae3ac7a5e98a280b
kerolous samy <keroloussamy98@gmail.com>
2020-04-24 23:45:09 +0200

    AddProductControllerandEditAndInde

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models.ViewModels;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hosting;
+
+        //with this property you don't need to add the model in posts methods becouse this property with attribute [BindProperty] binding all the data that come from user 
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; } 
+
+        public ProductsController(ApplicationDbContext db, IWebHostEnvironment hosting)
+        {
+            _db = db;
+            _hosting = hosting;
+            ProductsVM = new ProductsViewModel()
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                SpecialTags = _db.SpecialTags.ToList(),
+                Products = new Models.Products()
+            };
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
+            return View(await products.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            //here we send VM couse we need dropdown list in the view in Index we dont have dropdown list so we send Products
+            return View(ProductsVM);
+        }
+
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            _db.Products.Add(ProductsVM.Products);
+            await _db.SaveChangesAsync();
+
+
+            //Image begin saved
+            string webRootPath = _hosting.WebRootPath;//it just the pass for the project 
+            var files = HttpContext.Request.Form.Files;// will get the files that come from the View
+
+            var ProductsFromDb = _db.Products.Find(ProductsVM.Products.Id);
+
+            if (files.Count != 0)//this mean there a file , image has been uploaded
+            {
+                //Here we combine the project path with where i want to put the images
+                var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder);
+                //you have just one file(image) in all files that get from the view so the index will be zero [0]
+                var extension = Path.GetExtension(files[0].FileName);
+
+                //here we use fileStream to cope the file from uplouded to the server (your project)
+                using(var fileStream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))//will create the file on the server
+                {
+                    files[0].CopyTo(fileStream);
+                }
+
+                ProductsFromDb.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + extension;
+            }
+            else
+            {
+                //if didn't upload an image
+                var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder + @"\" + StaticDetails.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + ".png");
+                ProductsFromDb.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + ".png";
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) { return NotFound(); }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).SingleOrDefaultAsync(m => m.Id == id);
+
+            if(ProductsVM.Products == null) { return NotFound(); }
+
+            return View(ProductsVM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hosting.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                var productFromDb = _db.Products.Where(m => m.Id == id).FirstOrDefault();
+
+                if (files.Count > 0 && files[0] != null)
+                {
+                    //if user uploads a new image
+                    var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder);
+                    var extension_new = Path.GetExtension(files[0].FileName);
+                    var extension_old = Path.GetExtension(productFromDb.Image);
+
+                    if (System.IO.File.Exists(Path.Combine(uploads, ProductsVM.Products.Id + extension_old)))
+                    {
+                        System.IO.File.Delete(Path.Combine(uploads, ProductsVM.Products.Id + extension_old));
+                    }
+                    using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension_new), FileMode.Create))
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+                    ProductsVM.Products.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductsVM.Products.Id + extension_new;
+                }
+
+                if (ProductsVM.Products.Image != null)
+                {
+                    productFromDb.Image = ProductsVM.Products.Image;
+                }
+
+                productFromDb.Name = ProductsVM.Products.Name;
+                productFromDb.Price = ProductsVM.Products.Price;
+                productFromDb.Available = ProductsVM.Products.Available;
+                productFromDb.ProductTypeId = ProductsVM.Products.ProductTypeId;
+                productFromDb.SpecialTagsId = ProductsVM.Products.SpecialTagsId;
+                productFromDb.ShadeColor = ProductsVM.Products.ShadeColor;
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ProductsVM);
+        }
+
+
+    }
+}
---
+++ GraniteHouse/Utility/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Utility
+{
+    public class StaticDetails
+    {
+        public const string DefaultProductImage = "default_image.png";
+        public const string ImageFolder = @"Image\ProductImage";
+    }
+}
